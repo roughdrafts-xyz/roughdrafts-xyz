c91a1b97b47dea3f9746d1b98d5cf984d70da6a5
Minty <kinostl@gmail.com>
2022-05-27 11:09:07 -0400

    Thoughts going in a good direction

--- handlers/users/dashboard.js
+++ handlers/users/dashboard.js
@@ -1,21 +1,65 @@
 const { render, redirect } = require('server/reply')
 const prisma = require('../../prisma')
 const nanoid = require('../../nanoid')
+const md = require('markdown-it')()
+  .use(require('markdown-it-emoji'))
+  .use(require('markdown-it-highlightjs'))
+  .use(require('markdown-it-external-links'))
+  .disable(['heading', 'lheading'])
 
 const viewDashboard = async ctx => {
   const user = await prisma.user.findUnique({
-    where: { displayId: ctx.params.displayId }
+    where: { displayId: ctx.params.displayId },
+    include: {
+      thoughts: {
+        orderBy: {
+          updatedAt: 'desc'
+        }
+      }
+    }
   })
   const newArticleId = await nanoid()
 
   return render('dashboard', {
     ...user,
-    thoughts: [],
     newArticleId
   })
 }
 
+const extractTags = rawContent =>
+  rawContent
+    .split(' ')
+    .filter(o => o.startsWith('#'))
+    .map(o => o.slice(1))
+
+const doTagsHack = rawContent =>
+  rawContent
+    .split(' ')
+    .map(o => {
+      if (o.startsWith('#')) {
+        return `[${o}](/${o})`
+      }
+      return o
+    })
+    .join(' ')
+
 const addThought = async ctx => {
+  const rawContent = ctx.body.thought
+  const tags = extractTags(rawContent).map(curr => ({ displayId: curr }))
+  const tagHackedContent = doTagsHack(rawContent)
+  const content = md.render(tagHackedContent)
+
+  await prisma.thought.create({
+    data: {
+      content,
+      rawContent,
+      authorDisplayId: ctx.params.displayId,
+      tags: {
+        create: tags
+      }
+    }
+  })
+
   return redirect(`/@${ctx.params.displayId}/thoughts`)
 }
 
