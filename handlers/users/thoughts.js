c01b933a1899ee336b444308fbf902bd87373bcb
Minty <kinostl@gmail.com>
2022-05-28 10:12:12 -0400

    Thought editing

--- handlers/users/thoughts.js
+++ handlers/users/thoughts.js
@@ -7,7 +7,7 @@ const md = require('markdown-it')()
   .use(require('markdown-it-external-links'))
   .disable(['heading', 'lheading'])
 
-const shouldDisplay = (ctx, thought) => {
+const shouldAccess = (ctx, thought) => {
   if (!thought) return true
 
   return ctx.session?.user?.displayId === thought.authorDisplayId
@@ -23,37 +23,79 @@ const viewById = async ctx => {
     }
   })
 
-  if (!shouldDisplay(ctx, thought)) throw new Error('Illegal Action')
-  console.log(thought)
+  if (!shouldAccess(ctx, thought)) throw new Error('Illegal Action')
 
   return render('thoughts/view', thought)
 }
 
 const viewByTag = async ctx => {}
 
-const edit = async ctx => {
+const getThoughtEditor = async ctx => {
+  const thought = await prisma.thought.findUnique({
+    where: {
+      slugId: {
+        authorDisplayId: ctx.params.authorDisplayId,
+        displayId: ctx.params.displayId
+      }
+    }
+  })
+
+  const isInline = ctx.query.isInline === 'true' ? '?isInline=true' : ''
+
+  console.log('isInline', isInline)
+  if (!shouldAccess(ctx, thought)) throw new Error('Illegal Action')
+  return render('thoughts/edit', {
+    ...thought,
+    isInline
+  })
+}
+
+const updateThought = async ctx => {
+  const thought = await prisma.thought.findUnique({
+    where: {
+      slugId: {
+        authorDisplayId: ctx.params.authorDisplayId,
+        displayId: ctx.params.displayId
+      }
+    }
+  })
+
+  if (!shouldAccess(ctx, thought)) throw new Error('Illegal Action')
+
   const rawContent = ctx.body.thought
   const tags = extractTags(rawContent).map(curr => ({ displayId: curr }))
   const tagHackedContent = doTagsHack(ctx)
   const content = md.render(tagHackedContent)
+  const { displayId, authorDisplayId } = ctx.params
 
-  await prisma.thought.create({
+  await prisma.thought.update({
+    where: {
+      slugId: {
+        authorDisplayId,
+        displayId
+      }
+    },
     data: {
       content,
       rawContent,
-      displayId,
-      authorDisplayId: ctx.params.displayId,
       tags: {
         create: tags
       }
     }
   })
 
-  return redirect(`/@${ctx.params.displayId}/thoughts`)
+  if (ctx.query.isInline === 'true') {
+    return redirect(`/@${ctx.params.authorDisplayId}/thoughts`)
+  } else {
+    return redirect(
+      `/@${ctx.params.authorDisplayId}/thoughts/${ctx.params.displayId}`
+    )
+  }
 }
 
 module.exports = {
-  edit,
+  getThoughtEditor,
+  updateThought,
   viewById,
   viewByTag
 }
--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,18 @@ const handlers = require('auto-load')('handlers/users')
 
 module.exports = [
   get('/@:displayId', handlers.view.viewUser),
-  get('/@:displayId/thoughts/tagged/:tag', handlers.thoughts.viewByTag),
+  get('/@:authorDisplayId/thoughts/tagged/:tag', handlers.thoughts.viewByTag),
   get('/@:authorDisplayId/thoughts/:displayId', handlers.thoughts.viewById),
-  get('/@:displayId/thoughts/:thoughDisplayId/edit', handlers.thoughts.edit),
+  get(
+    '/@:authorDisplayId/thoughts/:displayId/edit',
+    handlers.thoughts.getThoughtEditor
+  ),
+  post(
+    '/@:authorDisplayId/thoughts/:displayId/edit',
+    handlers.thoughts.updateThought
+  ),
   get('/@:displayId/thoughts', handlers.dashboard.viewDashboard),
-  post('/@:displayId/thoughts', handlers.dashboard.addThought),
+  post('/@:authorDisplayId/thoughts', handlers.dashboard.addThought),
   get('/@:displayId/:displayId_notes.zip', handlers.view.downloadArticles),
   post('/@:displayId/delete', handlers.settings.deleteUser),
   get('/settings', handlers.settings.viewSettings),
